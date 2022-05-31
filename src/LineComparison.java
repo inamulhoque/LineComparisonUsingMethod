deea2384e7844e3b9d86bf57fd3b2375c2f83702
Inamul <inamulhoque097@gmail.com>
2022-05-31 19:14:54 +0530

    Added: Line1 is longer, shorter or equal to line2 and vice-versa.

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,8 +1,7 @@
 import java.util.Objects;
 
 public class LineComparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+    public static void compareTo(){
         int x1 = (int) Math.floor(Math.random()*10);
         int y1 = (int) Math.floor(Math.random()*10);
         int x2 = (int) Math.floor(Math.random()*10);
@@ -20,11 +19,16 @@ public class LineComparison {
         System.out.println("Generated first co-ordinates are: ("+x3+","+y3+") ("+x4+","+y4+")");
         double lenOfLine2 = Math.sqrt(Math.pow((x4-x3),2) + Math.pow((y4-y3),2) );
         System.out.println("Length of line2: "+lenOfLine2);
-
-        if (Objects.equals(lenOfLine1, lenOfLine2)) {
-            System.out.println("Lines are equal.");
+        if (lenOfLine1 == lenOfLine2){
+            System.out.println("Line1 = Line2");
+        } else if (lenOfLine1>lenOfLine2) {
+            System.out.println("Line1 is longer than line2.");
         } else {
-            System.out.println("Lines are not equal.");
+            System.out.println("Line2 is longer than line1.");
         }
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+        compareTo();
     }
 }
