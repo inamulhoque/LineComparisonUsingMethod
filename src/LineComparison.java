ed5adac32dce11a89b9d4eecc53340ae0a397ecf
Inamul <inamulhoque097@gmail.com>
2022-05-31 18:04:37 +0530

    Added: Generated co-ordinates randomly and find length of line.

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,5 +1,12 @@
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+        int x1 = (int) Math.floor(Math.random()*10);
+        int y1 = (int) Math.floor(Math.random()*10);
+        int x2 = (int) Math.floor(Math.random()*10);
+        int y2 = (int) Math.floor(Math.random()*10);
+        System.out.println("Generated co-ordinates are: ("+x1+","+y1+") ("+x2+","+y2+")");
+        double lenOfLine = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
+        System.out.println("Length of line: "+lenOfLine);
     }
 }
